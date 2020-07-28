85be8ca4793af9332b4b3a8b5d61592cc5b33b8e
Sean Rathbun <seanfrathbun@gmail.com>
2020-07-27 22:20:08 -0400

    Creating App

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Website for Justin Perry
         </a>
       </header>
     </div>
