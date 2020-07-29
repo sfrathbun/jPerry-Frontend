dd519e786438b15cc5f1992c6a6becc54c13a640
Sean Rathbun <seanfrathbun@gmail.com>
2020-07-28 20:33:21 -0400

    added simple file structure

---
+++ src/components/layouts/footer/Footer.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Footer extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="Footer">
+    
+      </div>
+    )
+  }
+}
+Footer.propTypes = {
+}
+
+export default Footer;
---
+++ src/components/layouts/nav/Nav.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Nav extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="Nav">
+    
+      </div>
+    )
+  }
+}
+Nav.propTypes = {
+}
+
+export default Nav;
