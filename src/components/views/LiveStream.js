3f619ce47c891bb653d066656e9d0986f1c7411d
Sean Rathbun <seanfrathbun@gmail.com>
2020-08-31 20:06:59 -0400

    trying to add embed code to see if I can go live

--- src/components/views/LiveStream.js
+++ src/components/views/LiveStream.js
@@ -10,7 +10,7 @@ const LiveStream = () => {
         <ReactPlayer
           className="react-player"
           controls={true}
-          url="https://www.youtube.com/watch?v=JxKjgZywKKo"
+          url="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Frathbun.sean%2Fvideos%2F10157621674933434%2F&width=1280"
           width="100%"
           height="100%"
           >
