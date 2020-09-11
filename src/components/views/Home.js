34dc2a8cd236c2bcf7c4158ebdeccc3cfc92173d
Sean Rathbun <seanfrathbun@gmail.com>
2020-09-10 21:30:25 -0400

    added slider to home

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { Carousel } from 'react-bootstrap';
+import bible from '../assets/home/bible.png'
 import PropTypes from 'prop-types';
 import '../../App.css';
 import '../../tailwind.output.css';
+
+
 const Home = () => {
 
   return (
     <div className="Home">
-      This is the Home page!
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={bible}
+                        alt="First slide" />
+                </Carousel.Item>
+            </Carousel>
+  
     </div>
   )
 };
